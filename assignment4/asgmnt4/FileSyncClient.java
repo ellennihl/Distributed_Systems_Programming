43c74ed86ea28dbb8b1300e0c076fb4c26c1ecac
Ellen <50663696+ellennihl@users.noreply.github.com>
2021-12-09 15:30:06 +0100

    Add files via upload

--- assignment4/asgmnt4/FileSyncClient.java
+++ assignment4/asgmnt4/FileSyncClient.java
@@ -7,90 +7,21 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.ObjectStreamClass;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-public class FileSyncClient implements Serializable {
-
-	private static final long serialVersionUID = -2662687341117920496L;
-
-	private String fileName;
-	private long modifiedDate;
-	private long sizeBytes;
-
-	private Set<File> files = new HashSet<File>();
-	//private File file;
-
-	public FileSyncClient() {
-		// file = f;
-		
-
-	}
-
-	public void addFile(File f) {
-		files.add(f);
-	}
-	//Det är nåt fel med iterator!!!!!!!!!!!!!!!!! det e därför min return e konstig
-	public String toString() {
-		/*String ret = "";
-		int i = 0;
-		Iterator<File> fileIt = files.iterator();
-		while (fileIt.hasNext()) {
-			File f = fileIt.next();
-			ret = ret + "File " + i++ + ": \nfileName="
-			+ f.getName() + "mod=" + f.lastModified() + "size=" + f.length() + "\n";
-		}
-		return ret;*/
-		
-		return "fileName="
-			+ fileName + "mod=" + modifiedDate + "size=" + sizeBytes + "\n";
-	}
-
-	private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-		while(in.available()>0) {
-			System.out.println("hej");
-			fileName = in.readUTF();
-			modifiedDate = in.readLong();
-		}
-		// sizeBytes = in.readLong();
-		// dateOpened = new Date();
-		// dateOpened.setTime(in.readLong());
-	}
-
-	private void writeObject(ObjectOutputStream out) throws IOException {
-		Iterator<File> fileIt = files.iterator();
-		while (fileIt.hasNext()) {
-			File f = fileIt.next();
-			System.out.println("hej2");
-			out.writeUTF(f.getName());
-			out.writeLong(f.lastModified());
-			// out.writeLong(fileIt.next().length());
-		}
-	}
+public class FileSyncClient  {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-		File newFile = new File(
-				"C:\\Users\\ellen\\eclipse-workspace\\Distributed Systems"
-		+ " Programming\\src\\asgmnt4\\test2.txt");
-		File newFile2 = new File(
-				"C:\\Users\\ellen\\eclipse-workspace\\Distributed Systems"
-		+ " Programming\\src\\asgmnt4\\test1.txt");
-		System.out.println(newFile.getName() + " " + newFile.length() + " " + newFile.lastModified());
-		if (newFile.createNewFile()) {
-			System.out.println("new file created: " + newFile.getName());
-		}
-
-		FileSyncClient fsc = new FileSyncClient();
-		fsc.addFile(newFile);
-		fsc.addFile(newFile2);
-		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("user.dat"));
-
-		out.writeObject(fsc);
-
-		out.close();
-
+		ObjectInputStream in = new ObjectInputStream(new FileInputStream("user.dat"));
+		FileSyncServer myDetailsFromDisk = (FileSyncServer)in.readObject();
+        System.out.println(myDetailsFromDisk);
+        //I need to verify the client by looking at its serialversionuid. 
+        long hej = ObjectStreamClass.lookup(myDetailsFromDisk.getClass()).getSerialVersionUID();
+        System.out.println(hej); 
 	}
 }
--- assignment4/asgmnt4/FileSyncServer.java
+++ assignment4/asgmnt4/FileSyncServer.java
@@ -1,19 +1,103 @@
 package asgmnt4;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.ObjectStreamClass;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
-public class FileSyncServer {
-	
-	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-		ObjectInputStream in = new ObjectInputStream(new FileInputStream("user.dat"));
-		FileSyncClient myDetailsFromDisk = (FileSyncClient)in.readObject();
-        System.out.println(myDetailsFromDisk);
-        //I need to verify the client by looking at its serialversionuid. 
-        long hej = ObjectStreamClass.lookup(myDetailsFromDisk.getClass()).getSerialVersionUID();
-        System.out.println(hej); 
+public class FileSyncServer implements Serializable{
+
+	private static final long serialVersionUID = -2662687341117920496L;
+
+	private String fileName;
+	private long modifiedDate;
+	private long sizeBytes;
+
+	private Set<File> files;
+	//+-private File file;
+
+	public FileSyncServer() {
+		files = new HashSet<File>();
+		
 	}
+
+	public void addFile(File f) {
+		files.add(f);
+	}
+
+	public String toString() {
+		/*String ret = "";
+		int i = 0;
+		Iterator<File> fileIterator = files.iterator();
+		while (fileIterator.hasNext()) {
+			File f = fileIterator.next();
+			ret = ret + "File " + i++ + ": \nfileName="
+			+ f.getName() + "mod=" + f.lastModified() + "size=" + f.length() + "\n";
+		}
+		return ret;*/
+		return "fileName="
+				+ fileName + "mod=" + modifiedDate + "size=" + sizeBytes + "\n";
+	}
+
+	private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+		if(files==null)
+			System.out.println("innan read");
+		while(in.available()>0) {
+			System.out.println("hej");
+			fileName = in.readUTF();
+			modifiedDate = in.readLong();
+		}
+		if(files==null)
+			System.out.println("efter read");
+		// sizeBytes = in.readLong();
+		// dateOpened = new Date();
+		// dateOpened.setTime(in.readLong());
+	}
+
+	private void writeObject(ObjectOutputStream out) throws IOException {
+		Iterator<File> fileIt = files.iterator();
+		if(files==null)
+			System.out.println("innan iterate");
+		while (fileIt.hasNext()) {
+			File f = fileIt.next();
+			System.out.println("hej2");
+			out.writeUTF(f.getName());
+			out.writeLong(f.lastModified());
+			// out.writeLong(fileIt.next().length());
+		}
+		if(files==null)
+			System.out.println("efter iterate");
+	}
+
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+		File newFile = new File(
+				"C:\\Users\\ellen\\eclipse-workspace\\Distributed Systems"
+		+ " Programming\\src\\asgmnt4\\test2.txt");
+		File newFile2 = new File(
+				"C:\\Users\\ellen\\eclipse-workspace\\Distributed Systems"
+		+ " Programming\\src\\asgmnt4\\test1.txt");
+		System.out.println(newFile.getName() + " " + newFile.length() + " " + newFile.lastModified());
+		if (newFile.createNewFile()) {
+			System.out.println("new file created: " + newFile.getName());
+		}
+
+		FileSyncServer fsc = new FileSyncServer();
+		fsc.addFile(newFile);
+		fsc.addFile(newFile2);
+		ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("user.dat"));
+
+		out.writeObject(fsc);
+
+		out.close();
+
+	}
+
 }
