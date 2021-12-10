5b8fee1791c4f336e00b69f8385477670a48d654
Eekdeb <50443198+Eekdeb@users.noreply.github.com>
2021-12-10 16:46:03 +0100

    Update Server.java

--- Remote*File List*Sync T4/Server.java
+++ Remote*File List*Sync T4/Server.java
@@ -1,7 +1,6 @@
 package Task4;
 
 import java.io.EOFException;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -26,13 +25,6 @@ public class Server {
 	}
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-		/*
-		
-		}*/
-		/*ObjectInputStream in = new ObjectInputStream(new FileInputStream("user.dat"));
-		ObjectOutputStream out = null;*/
-		//ArrayList<FileObj> database = getFromDat(in);
-		
 		ArrayList<FileObj> database = new ArrayList<FileObj>();
 		ArrayList<FileObj> newData = new ArrayList<FileObj>();
 		Socket client = null;
@@ -44,34 +36,45 @@ public class Server {
 			ServerSocket server = new ServerSocket(8888); // start listening on port 8888
 			client = server.accept(); // this method is a blocking I/O call, it will not be called unless
 			//a connection is established.
-			out = new ObjectOutputStream(client.getOutputStream()); // get the output stream of client.
-			in = new ObjectInputStream(client.getInputStream());    // get the input stream of client.
 			
-			try {
-				while(true) {
-					newData.add((FileObj)in.readObject());
-				}
-			}
-			catch(EOFException exc){
-				//TODO Gämför och skicka tillbaka
+			out = new ObjectOutputStream(client.getOutputStream()); // get the output stream of client.
+			in = new ObjectInputStream(client.getInputStream());   // get the input stream of client.
+			
+			newData = (ArrayList<FileObj>)in.readObject();
+				//TODO compare of thay are the same
+				ArrayList<FileObj> different = new ArrayList<FileObj>();
+				ArrayList<FileObj> toRemove = new ArrayList<FileObj>();
+				ArrayList<FileObj> same = new ArrayList<FileObj>();
 				for (FileObj f : newData) {
 					for (FileObj db : database) {
 						if(f.getName().equals(db.getName())) {
 							if(f.getLastUpdate().compareTo(db.getLastUpdate()) == 0) {
-								System.out.println("same");
+								System.out.println("same " + f.getName());
+								same.add(f);
 							} else {
-								System.out.println("different or new");
+								System.out.println("different " + f.getName());
+								different.add(f);
+								toRemove.add(db);
 							}
 						}
 					}
 				}
 				
+				database.removeAll(toRemove);
+				newData.removeAll(same);
+				newData.removeAll(different);
+				database.addAll(different);
 				
-				for (FileObj fileObj : database) {
-					System.out.println(fileObj);
+				if(!newData.isEmpty()) {
+					for (FileObj f : newData) {
+						System.out.println("New file " + f);
+						database.add(f);
+						different.add(f);
+					}
 				}
-				System.out.println("End");
-			}
+				System.out.println("End \r\n \r\n");
+			
+				out.writeObject(different);
 			
 			// close resources
 			out.close();
@@ -79,7 +82,11 @@ public class Server {
 			client.close();
 			server.close();
 			newData.clear();
+			toRemove.clear();
+			different.clear();
+			same.clear();
 			}
+			
 			} catch (IOException e) {
 			e.printStackTrace();
 			System.exit(-1);
