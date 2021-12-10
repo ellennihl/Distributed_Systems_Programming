da220fbe5a77dca59131abc2225e9032ae27ba51
Ellen <50663696+ellennihl@users.noreply.github.com>
2021-12-10 16:39:52 +0100

    Add files via upload

--- assignment4/asgmnt4/FileSyncClient.java
+++ assignment4/asgmnt4/FileSyncClient.java
@@ -1,127 +1,64 @@
 package asgmnt4;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.ObjectStreamClass;
-import java.io.Serializable;
 import java.net.Socket;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.ArrayList;
+public class FileSyncClient {
 
-import asgmt2.Server;
-
-public class FileSyncClient implements Serializable {
-
-	private static final long serialVersionUID = -2662687341117920496L;
-	
-	private String fileName;
-	private long modifiedDate;
-	private long sizeBytes;
-
-	private Set<File> files;
-	//private File file;
+	Socket s;
+	ObjectInputStream in;
+	ObjectOutputStream out;
+	public ArrayList<FileObj> files;
 
 	public FileSyncClient() {
-		files = new HashSet<File>();
-	}
-
-	public void addFile(File f) {
-		files.add(f);
-	}
-
-	public String toString() {
-		/*String ret = "";
-		int i = 0;
-		Iterator<File> fileIterator = files.iterator();
-		while (fileIterator.hasNext()) {
-			File f = fileIterator.next();
-			ret = ret + "File " + i++ + ": \nfileName="
-			+ f.getName() + "mod=" + f.lastModified() + "size=" + f.length() + "\n";
-		}
-		return ret;*/
-		return "fileName="
-				+ fileName + "mod=" + modifiedDate + "size=" + sizeBytes + "\n";
-	}
-
-	private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-		if(files==null)
-			System.out.println("innan read");
-		while(in.available()>0) {
-			System.out.println("hej");
-			fileName = in.readUTF();
-			modifiedDate = in.readLong();
-		}
-		if(files==null)
-			System.out.println("efter read");
-		// sizeBytes = in.readLong();
-		// dateOpened = new Date();
-		// dateOpened.setTime(in.readLong());
-	}
-
-	private void writeObject(ObjectOutputStream out) throws IOException {
-		Iterator<File> fileIt = files.iterator();
-		if(files==null)
-			System.out.println("innan iterate");
-		while (fileIt.hasNext()) {
-			File f = fileIt.next();
-			System.out.println("hej2");
-			out.writeUTF(f.getName());
-			out.writeLong(f.lastModified());
-			// out.writeLong(fileIt.next().length());
-		}
-		if(files==null)
-			System.out.println("efter iterate");
-	}
-	
-	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
-
-		Socket s = null;
-		ObjectInputStream in = null;
-		ObjectOutputStream out = null;
 		try {
 			s = new Socket("localhost", FileSyncServer.PORT);
-			//in = new ObjectInputStream(s.getInputStream());
 			out = new ObjectOutputStream(s.getOutputStream());
-					//new FileOutputStream("user.dat"));//new ObjectOutputStream(s.getOutputStream());
+			in = new ObjectInputStream(s.getInputStream());
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		File newFile = new File(
-				"C:\\Users\\ellen\\eclipse-workspace\\Distributed Systems"
-		+ " Programming\\src\\asgmnt4\\test2.txt");
-		File newFile2 = new File(
-				"C:\\Users\\ellen\\eclipse-workspace\\Distributed Systems"
-		+ " Programming\\src\\asgmnt4\\test1.txt");
-		System.out.println(newFile.getName() + " " + newFile.length() + " " + newFile.lastModified());
-		if (newFile.createNewFile()) {
-			System.out.println("new file created: " + newFile.getName());
-		}
-
-		FileSyncClient fsc = new FileSyncClient();
-		System.out.println("new client");
-		fsc.addFile(newFile);
-		fsc.addFile(newFile2);
-		//ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("user.dat"));
-		
-		out.writeObject(fsc);
-		System.out.println("writeobj");
-		s.close();
-		out.close();
-
+		files = new ArrayList<FileObj>();
 	}
 	
-
+	public void addFile(File f) throws IOException {
+		FileObj fo = new FileObj(f);
+		if (f.createNewFile()) {
+			System.out.println("new file created: " + f.getName());
+		}
+		files.add(fo);
+		
+	}
 	
+	public void sync() throws IOException, ClassNotFoundException, InterruptedException {
+		out.writeObject(files);
+		ArrayList<FileObj> backup = (ArrayList<FileObj>) in.readObject();
+		System.out.println(backup);
 
+		s.close();
+		out.close();
+		in.close();
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException, InterruptedException {
+
+		FileSyncClient fsc = new FileSyncClient();
+
+		File dir = new File("C:\\Users\\ellen\\eclipse-workspace\\Distributed Systems" + 
+		" Programming\\src\\asgmnt4\\files");
+		File[] dirContents = dir.listFiles();
+		if (dirContents != null) {
+		  for (File child : dirContents) {
+			  fsc.addFile(child);
+		  }
+		}
+		
+		fsc.sync();
+
+	}
 }
--- assignment4/asgmnt4/FileSyncServer.java
+++ assignment4/asgmnt4/FileSyncServer.java
@@ -1,12 +1,12 @@
 package asgmnt4;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectStreamClass;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class FileSyncServer {
 	public static final int PORT = 7896;
@@ -14,29 +14,44 @@ public class FileSyncServer {
 	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
 		ServerSocket s = null;
 		Socket client = null;
-		try {
-			s = new ServerSocket(PORT);
-			client = s.accept();
-			System.out.println("accepted");
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-		ObjectInputStream in = new ObjectInputStream(client.getInputStream());
-				//new ObjectInputStream(new FileInputStream("user.dat"));//client.getInputStream());
-		//ObjectOutputStream out = new ObjectOutputStream(client.getOutputStream());
-		System.out.println("hej");
-		//ObjectInputStream in = new ObjectInputStream(new FileInputStream("user.dat"));
-		FileSyncClient myDetailsFromDisk = (FileSyncClient)in.readObject();
-        System.out.println(myDetailsFromDisk);
-        //I need to verify the client by looking at its serialversionuid. 
-        long hej = ObjectStreamClass.lookup(myDetailsFromDisk.getClass()).getSerialVersionUID();
-        System.out.println(hej); 
-        
-        s.close();
-        client.close();
-        in.close();
-        
-	}
-	
+		ObjectOutputStream out = null;
+		ObjectInputStream in = null;
+		ArrayList<FileObj> db = new ArrayList<FileObj>();
+		ArrayList<FileObj> syncDb = new ArrayList<FileObj>();
+		while (true) {
+			try {
+				s = new ServerSocket(PORT);
+				client = s.accept();
+				System.out.println("accepted");
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+			}
+			out = new ObjectOutputStream(client.getOutputStream());
+			in = new ObjectInputStream(client.getInputStream());
+			ArrayList<FileObj> fo = (ArrayList<FileObj>) in.readObject();
+			for (FileObj readObj : fo) {
+				boolean found = false;
+				for (FileObj dbObj : db) {
+					if (readObj.equals(dbObj)) {
+						found = true;
+						break;
+					}
+				}
+				if (!found) {
+					syncDb.add(readObj);
+					db.add(readObj);
+				}
 
+			}
+			System.out.println("sync:\n" + syncDb);
+			out.writeObject(syncDb);
+			syncDb.clear();
+
+			s.close();
+			client.close();
+			out.close();
+			in.close();
+		}
+
+	}
 }
